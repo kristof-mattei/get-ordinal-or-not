b731a30ee8f3653f6b5eac5c80d997d4230ac568
Kristof Mattei <delegatevoid@gmail.com>
2014-05-24 14:06:39 +0200

    Initial commit

---
+++ GetOrdinalTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿namespace GetOrdinalTests
+{
+    using System.Diagnostics;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Debug.WriteLine("TestInitialize");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Debug.WriteLine("TestCleanup");
+        }
+    }
+}
